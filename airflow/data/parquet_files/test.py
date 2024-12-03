5aebecb92b13d4959fd8ec9140add9e8152dba92
eddamnatikram <eddmnatiikram123@gmail.com>
2024-12-03 21:29:12 +0100

    Second

--- airflow/data/parquet_files/test.py
+++ airflow/data/parquet_files/test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Path to the Parquet file
-file_path = 'openmeteo_data.parquet'
+file_path = './parquet_file/openmeteo_data.parquet'
 
 # Read and display the Parquet file
 df = pd.read_parquet(file_path)
